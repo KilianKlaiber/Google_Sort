bdea77f13496f749657383a96344f6743fa58435
KilianKlaiber <kilianklaiber@gmail.com>
2025-04-15 09:00:25 +0200

    error handling in external functions

--- list.py
+++ list.py
@@ -1,31 +1,35 @@
+"""Order the list with the minimum number of swaps of adjacent elements:
+
+Explanation: Swapping the largest and shortest numbers to the beginning
+and ending does not affect the relative order of the residual numbers.
+Each swap is necessary, in order to bring the largest and smallest numbers
+to their final position. Since the algorithm never perform an unnecessary swap,
+it reaches the final solution with the least number of swaps.
+
+
+If the elements in the list are not unique then the smallest index for the smallest number
+, which is closest to the beginning, is chosen and vice versa
+for the largest number closest to the end. This guarantees that the algorithm
+also uses the minimum amount of swaps for lists with duplicates.
+"""
+
+
 def main():
+    """Execute recursive ordering function"""
 
     unordered_list = [2, 7, 3, 8, 7, 7, 5, 2, 1, 4, 5, 4, 3, 5, 6, 8]
 
     ordered_list = recursive_reordering(unordered_list)
 
-    print("ordered list", ordered_list)
+    print("ordered list: ", ordered_list)
 
 
 def recursive_reordering(unordered_list: list[int]) -> list[int]:
-    """Order the list with the minimum number of swaps of adjacent elements:
-
-    Explanation: Swapping the largest and shortest numbers to the beginning
-    and ending does not affect the relative order of the residual numbers.
-    Each swap is necessary, in order to bring the largest and smallest numbers
-    to their final position. Since the algorithm never perform an unnecessary swap,
-    it reaches the final solution with the least number of swaps.
-
-
-    If the elements in the list are not unique then the smallest index for the smallest number
-    , which is closest to the beginning, is chosen and vice versa
-    for the largest number closest to the end. This guarantees that the algorithm
-    also uses the minimum amount of swaps for lists with duplicates.
-
-    Returns:
-        list[int]: ordered list of integers.
     """
-    print("unordered_list", unordered_list)
+    Return list of ordered integers.
+    """
+
+    handle_list_error(unordered_list)
 
     # Exit conditions for recursion
 
@@ -47,32 +51,36 @@ def recursive_reordering(unordered_list: list[int]) -> list[int]:
     if smallest_number_distance <= largest_number_distance:
         for index in range(smallest_number_distance - 1, -1, -1):
             unordered_list = swap(unordered_list, index)
+
         if len(unordered_list) >= 2:
-            first_ordered_index = [unordered_list[0]]
-            return first_ordered_index + recursive_reordering(unordered_list[1:])
+            return [unordered_list[0]] + recursive_reordering(unordered_list[1:])
         else:
             return unordered_list
 
     else:
         for index in range(largest_number_index, last_index):
             unordered_list = swap(unordered_list, index)
-        last_ordered_index = [unordered_list[-1]]
-        unordered_list_without_last_index = unordered_list[:last_index]
+
         if len(unordered_list) >= 2:
-            return (
-                recursive_reordering(unordered_list_without_last_index)
-                + last_ordered_index
-            )
+            return recursive_reordering(unordered_list[:last_index]) + [
+                unordered_list[-1]
+            ]
         else:
             return unordered_list
 
 
 def largest_number(unordered_list: list[int]) -> int:
     """Return index of largest number in list"""
+
+    handle_list_error(unordered_list)
+
     largest_number = unordered_list[0]
     largest_number_index = 0
 
     for index, value in enumerate(unordered_list):
+
+        handle_value_error(value)
+
         # >= instead of > in order to get the last index, if the largest_number is not unique
         if value >= largest_number:
             largest_number = value
@@ -84,12 +92,16 @@ def largest_number(unordered_list: list[int]) -> int:
 def smallest_number(unordered_list: list[int]) -> int:
     """Return index of smallest number in list"""
 
+    handle_list_error(unordered_list)
+
     smallest_number = unordered_list[0]
     smallest_number_index = 0
 
     for index, value in enumerate(unordered_list):
+        handle_value_error(value)
         # < instead of <= in order to get the first index, if the smallest_number is not unique
         if value < smallest_number:
+
             smallest_number = value
             smallest_number_index = index
 
@@ -105,14 +117,8 @@ def ordered(unordered_list: list[int], index: int) -> bool:
 
     # Error Handling
 
-    if not isinstance(index, int):
-        raise TypeError("The index must be of type integer")
-
-    last_index = len(unordered_list) - 1
-    if index >= last_index or index < 0:
-        error_message = f"Your index {index} is out of bounds."
-
-        raise IndexError(error_message)
+    handle_list_error(unordered_list)
+    handle_index_error(unordered_list, index)
 
     # Ordering adjacent values
 
@@ -120,6 +126,9 @@ def ordered(unordered_list: list[int], index: int) -> bool:
     first_value = unordered_list[first_index]
     second_index = index + 1
     second_value = unordered_list[second_index]
+
+    handle_value_error(first_value)
+    handle_value_error(second_value)
 
     if second_value >= first_value:
         return True
@@ -136,15 +145,8 @@ def swap(unordered_list: list[int], index: int) -> list[int]:
 
     # Error Handling
 
-    if not isinstance(index, int):
-        raise TypeError("The index must be of type integer")
-
-    last_index = len(unordered_list) - 1
-    if index >= last_index or index < 0:
-
-        error_message = f"Your index {index} is out of bounds."
-
-        raise IndexError(error_message)
+    handle_list_error(unordered_list)
+    handle_index_error(unordered_list, index)
 
     # Swapping adjacent values:
 
@@ -157,6 +159,32 @@ def swap(unordered_list: list[int], index: int) -> list[int]:
     unordered_list[second_index] = first_value
 
     return unordered_list
+
+
+def handle_list_error(unordered_list):
+    """Raise type error if unordered list is not of type list"""
+    if not isinstance(unordered_list, list):
+        raise TypeError(f"Unordered list {unordered_list} must be of type list[int]")
+
+
+def handle_index_error(unordered_list, index):
+    "Raise Error is index is not integer or out of bounds"
+
+    if not isinstance(index, int):
+        raise TypeError(f"The index {index} must be of type integer")
+
+    last_index = len(unordered_list) - 1
+    if index >= last_index or index < 0:
+
+        error_message = f"Your index {index} is out of bounds."
+
+        raise IndexError(error_message)
+
+
+def handle_value_error(value):
+    """Raise error if value ist not of type int"""
+    if not isinstance(value, int):
+        raise TypeError(f"Element {value} must be of type int")
 
 
 if __name__ == "__main__":
